e7f3f41e6561ea521f12c5637c7dd2d16974cee6
Rohit <rohitchauhan2219@gmail.com>
2025-03-14 09:39:38 +0530

    Updated Files with no practical 5th and 6th

---
+++ IP Practical 6/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+// Lodash
+const _ = require('lodash');
+const numbers = [1, 2, 3, 4, 5];
+console.log("Shuffled Array:", _.shuffle(numbers));
+
+// Moment
+
+const moment = require('moment');
+console.log("Current Date and Time:", moment().format('YYYY-MM-DD HH:mm:ss'));
